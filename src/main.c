5ed7d54aa3bdea87c9f33e03171d9f599333cd7b
kyj93790 <kyj93790@naver.com>
2022-06-08 15:02:22 +0900

    feat: screen connection identifier 및 window 생성

--- src/main.c
+++ src/main.c
@@ -2,9 +2,11 @@
 
 int main(void)
 {
-	// void	*mlx_ptr;
-	// void	*win_ptr;
+	void	*mlx_ptr;
+	void	*win_ptr;
 
-	//
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 1024, 512, "raycaster");
+	mlx_loop(mlx_ptr);
 	return (0);
 }
